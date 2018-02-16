4fce468d7da5b5a852c40848c5497801a63238a0
jeromete <tessonjerome@gmail.com>
2018-02-16 22:19:00 +0100

    create a user model #9

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
+
+  def add_error(error)
+    self.error_message = self.error_message + ' ' + error
+  end
 end
